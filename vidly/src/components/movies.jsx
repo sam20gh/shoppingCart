930e4be362c0ca22d233b353a62e2e0daafa7730
github username <github email address>
2019-09-05 19:31:19 +0100

    MOvie DB added

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+	state = {
+		movies: getMovies()
+	};
+	handleDelete = (movie) => {
+		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+		this.setState({ movies });
+	};
+	render() {
+		const { length: count } = this.state.movies;
+
+		if (count === 0)
+			return (
+				<div class="alert alert-primary" role="alert">
+					{' '}
+					There are no Movies in this database{' '}
+				</div>
+			);
+
+		return (
+			<React.Fragment>
+				<p> Showing {count} mpovies in the database</p>
+				<table className="table">
+					<thead>
+						<th>Title</th>
+						<th>Genre</th>
+						<th>Stock</th>
+						<th>Rate</th>
+						<th />
+					</thead>
+					<tbody>
+						{this.state.movies.map((movie) => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td> {movie.numberInStock}</td>
+								<td> {movie.dailyRentalRate}</td>
+								<td>
+									<button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Movies;
