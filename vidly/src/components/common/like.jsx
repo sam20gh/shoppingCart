e4790098533dda3c6b8a125b3ea993692e033328
github username <github email address>
2019-09-20 00:02:13 +0100

    Change of like component

--- vidly/src/components/common/like.jsx
+++ vidly/src/components/common/like.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-	render() {
-		let classes = 'fa fa-heart';
-		if (!this.props.liked) classes += '-o';
-		return <i onClick={this.props.onClick} style={{ cursor: 'pointer' }} className={classes} aria-hidden="true" />;
-	}
-}
+const Like = (props) => {
+	let classes = 'fa fa-heart';
+	if (!props.liked) classes += '-o';
+	return <i onClick={props.onClick} style={{ cursor: 'pointer' }} className={classes} aria-hidden="true" />;
+};
 
 export default Like;
